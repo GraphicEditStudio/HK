d9b4c8327fb8c216cfb32ed28df8cf9b8de62276
algatrazcpt <53114980+algatrazcpt@users.noreply.github.com>
2022-05-14 21:19:41 +0300

    EnemiesPart1-v1

--- HK/Assets/Scripts/GameEnding.cs
+++ HK/Assets/Scripts/GameEnding.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
+    public static GameEnding instance;
     public float fadeDuration = 1f;
     public float displayImageDuration = 1f;
     public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
     float m_Timer;
     bool m_IsPlayerAtExit = false;
+    bool m_IsPlayerCaught=false;
+    private void Awake()
+    {
+        if(instance!=null)
+        {
+            return;
+        }
+        instance = this;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
@@ -17,20 +29,36 @@ public class GameEnding : MonoBehaviour
             return;
         }
     }
+    public void CaughtPlayer()
+    {
+        m_IsPlayerCaught = true;
+    }
     private void Update()
     {
+
         if (m_IsPlayerAtExit)
         {
-            EndLevel();
+            EndLevel(exitBackgroundImageCanvasGroup,false);
+        }
+        else if(m_IsPlayerCaught)
+        {
+            EndLevel(caughtBackgroundImageCanvasGroup,true);
         }
     }
-    void EndLevel()
+    void EndLevel(CanvasGroup canvasGroupValue,bool doRestart)
     {
         m_Timer += Time.deltaTime;
-        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+        canvasGroupValue.alpha = m_Timer / fadeDuration;
         if (m_Timer > fadeDuration + displayImageDuration)
         {
-            Application.Quit();
+            if (doRestart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit();
+            }
         }
     }
 }
